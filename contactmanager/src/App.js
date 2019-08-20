2e2a4d11430f0df6f0b5be355ed8d07b202db733
Christian Awelakoue <awelakoue@gmail.com>
2019-08-20 22:05:12 +0200

    create first react app

---
+++ contactmanager/src/App.js
@@ -0,0 +1,14 @@
+/* jshint esversion:6 */
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>The App Component</h1>
+      </div>
+    );
+  }
+}
+export default App;
