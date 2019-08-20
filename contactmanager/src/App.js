7d0bc36bb662f3e77314b889361ddb25cbc666d4
Christian Awelakoue <awelakoue@gmail.com>
2019-08-20 23:17:10 +0200

    JSX expressesions

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -4,9 +4,28 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const name = 'Kate';
+    const showHello = true;
+    const showMath = true;
+    const num1 = 40;
+    const num2 = 23;
+
+
+    let math;
+    if (showMath) {
+      // eslint-disable-next-line jsx-a11y/heading-has-content
+      math = (<h4>
+          {num1} + {num2} = {num1 + num2}
+      </h4>);
+    } else {
+      math = null;
+    }
     return (
       <div className="App">
         <h1>The App Component</h1>
+        {showHello ?
+          <h4>Hello {name.toUpperCase()}</h4> : null}
+        {math}
       </div>
     );
   }
